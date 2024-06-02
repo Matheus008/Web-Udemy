5c1445b97d2e53782fc84802f3b46d02210d3bc0
Matheus Wassermann <matheus5wassermann@gmail.com>
2024-06-02 20:55:38 -0300

    Atualização Webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,8 +1,9 @@
-const modoDev = process.env.NODE_ENV !== 'production'
-const webpack = require('webpack')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const modoDev = process.env.NODE_ENV !== 'production';
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TeserPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     mode: modoDev ? 'development' : 'production',
@@ -12,35 +13,46 @@ module.exports = {
         path: __dirname + '/public'
     },
     devServer: {
-        contentBase: "./public",
-        port: 9000
+        contentBase: './public',
+        port: 9000 
     },
     optimization: {
         minimizer: [
-            new UglifyJsPlugin({
+            /*new UglifyJsPlugin({
                 cache: true,
-                parallel: true
+                parallel: true,
+            }),*/ //não está funcionando corretamente substitui para o terser-webpack-plugin
+            new TeserPlugin({
+                parallel: true,
+                terserOptions: {
+                    ecma: 6,
+                }
             }),
-            new OptimizeCSSAssetsPlugin({})
+            new OptimizeCssAssetsPlugin({})
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: "estilo.css"
+            filename: "estilo.css",
         })
     ],
     module: {
-        rules: [{
-            test: /\.s?[ac]ss$/,
-            use: [
-                MiniCssExtractPlugin.loader,
-                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
-                'css-loader', // interpreta @import, url()...
-                'sass-loader',
-            ]
+        rules: [
+            {
+                test: /\.?[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    //'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                    'css-loader', // interpreta @import, url()
+                    'sass-loader', 
+                ]
         }, {
             test: /\.(png|svg|jpg|gif)$/,
             use: ['file-loader']
-        }]
+        }
+
+    ]
     }
 }
+
+// OBS: Caso o optimize não for testar o npm install css-minimizer-webpack-plugin --save-dev
